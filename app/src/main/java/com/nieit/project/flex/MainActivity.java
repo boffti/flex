b0bce180832e22a12a3b8be6a5faade39f89faba
Aneesh Melkot <boffti@gmail.com>
2016-02-13 21:38:07 +0530

    Initial Commit

---
+++ app/src/main/java/com/nieit/project/flex/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nieit.project.flex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
